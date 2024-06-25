8b43fd5676b04a84d79aa20346edc104e6fb9788
kennedy-dan <kenidankid@yahoo.com>
2024-06-25 23:02:33 +0100

    icon resize

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,33 +4,33 @@ const Banner = () => {
   return (
     <div className="bg-[#030c0f] text-white text-[10px] flex items-center md:justify-around justify-between w-full lg:px-20 px-4 py-10 mt-32">
       <div className="md:flex space-x-1 items-center ">
-      <img src="/powerbi.png" alt='' className="lg:w-20 lg:h-20 w-5 h-5 object-contain" />
+      <img src="/powerbi.png" alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
        
         <p className='md:block hidden' >PowerBi</p>
       </div>
       <div className="md:flex space-x-1 items-center ">
-      <img src="/python.png" alt='' className="lg:w-20 lg:h-20 w-5 h-5 object-contain" />
+      <img src="/python.png" alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
 
         <p className='md:block hidden'>Python</p>
       </div>
 
       <div className="md:flex space-x-1 items-center ">
-        <img src="/datalake.png " alt='' className="lg:w-20 lg:h-20 w-5 h-5object-contain" />
+        <img src="/datalake.png " alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
 
         <p className='md:block hidden'>Data Lake</p>
 
       </div>
       <div>
-        <img src="/sql.png" alt='' className="lg:w-20 lg:h-20 w-5 h-5 object-contain" />
+        <img src="/sql.png" alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
       </div>
       <div className="md:flex space-x-1 items-center ">
-        <img src="/bricks.png" alt='' className="lg:w-20 lg:h-20 w-5 h-5 object-contain" />
+        <img src="/bricks.png" alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
 
         <p className='md:block hidden'>Databricks</p>
       </div>
 
       <div className="md:flex space-x-1 items-center ">
-        <img src="/azure.png" alt='' className="lg:w-20 lg:h-20 w-5 h-5 object-contain" />
+        <img src="/azure.png" alt='' className="lg:w-20 lg:h-20 w-9 h-9 object-contain" />
 
         <p className='md:block hidden'>azure synapse analytics</p>
       </div>
